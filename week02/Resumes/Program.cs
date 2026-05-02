d1a1790f5e61b29fff3991b3840dd2909c2d9d42
RonaldDiaz <ronalddiazz@gmail.com>
2026-05-02 15:56:47 -0400

    W02 Learning Activity - Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "LUZ";
+        job1._jobTitle = "Professor";
+        job1._startYear = 2027;
+        job1._endYear = 2047;
+
+        Job job2 = new("BYU", "Student", 2025, 2028);
+
+        Resume resume1 = new();
+        resume1._name = "Ronald Díaz";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();      
     }
 }
