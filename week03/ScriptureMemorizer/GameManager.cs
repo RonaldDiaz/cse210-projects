51d11265cf008c941a1bc6ee8da6d7ae7062cb95
RonaldDiaz <ronalddiazz@gmail.com>
2026-05-23 14:11:20 -0400

    W03 Project: Scripture Memorizer Program

---
+++ week03/ScriptureMemorizer/GameManager.cs
@@ -0,0 +1,61 @@
+public class GameManager
+{
+    private int _level;
+    private Scripture _scripture;
+
+    public GameManager()
+    {
+        _scripture = GetScripture();
+        _level = SetLevel();
+    }
+
+    private static Scripture GetScripture()
+    {
+        Console.Write("Press 1 if you want to select an Scripture from a list, or Enter if you want a random Scripture: ");
+        string choice = Console.ReadLine();
+        int index = 0;
+        if (choice == "1")
+        {
+            Console.WriteLine(Scripture.GetScripturesMenu());
+            Console.WriteLine("Enter the number of the Scripture you want to memorize: ");
+            // If input is an integer, set it as the index, otherwise set the index as 0.
+            index = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+        }
+        return Scripture.GetScripture(index);
+    }
+    
+    private static int SetLevel()
+    {
+        Console.WriteLine("Select the difficulty level by choosing the number of words to hide each turn: ");
+        Console.WriteLine("1. Easy.");
+        Console.WriteLine("2. Moderate.");
+        Console.WriteLine("3. Hard.");
+        string input = Console.ReadLine();
+        // If input is an integer (TryParse) and is between 1 and 3, take the value. Otherwise, take 2 (Moderate).
+        return int.TryParse(input, out int result) && result > 0 && result < 4 ? result : 2;
+    }
+    
+    public void StartGame()
+    {
+        Console.Clear();
+        Console.WriteLine(_scripture.GetDisplayText());
+        bool quit = false;
+        while (!_scripture.IsCompletelyHidden())
+        {
+            Console.WriteLine("\nPress enter to continue or type 'quit' to finish.");
+            string input = Console.ReadLine().Trim().ToLower();
+            if (input.Equals("quit"))
+            {
+                quit = true;
+                break;
+            }
+            else
+            {
+                _scripture.HideRandomWords(_level);
+                Console.Clear();
+                Console.WriteLine(_scripture.GetDisplayText());
+            }
+        }
+        Console.WriteLine(quit ? "\nSee you next time!" : "\nCongratulations, you finish the exercise!");
+    }
+}
--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,28 @@
+// Enhancements: Added functionality to allow the user select a scripture from a list.
+// Added a choice to select game dificulty (number of words hidden per turn).
+// Added the class Game Manager to handle new functionality.
+// https://www.quora.com/How-do-I-generate-a-list-of-random-integers-without-repeating-in-C
+// https://imaginaformacion.com/tutoriales/strings-en-c-sharp
+// https://codemia.io/knowledge-hub/path/best_way_to_repeat_a_character_in_c
+// https://www.aprendeaprogramar.com/referencia/view.php?f=Console.Clear&leng=Csharp
+// https://code-maze.com/csharp-print-elements-of-an-array/
+// https://learn.microsoft.com/es-es/dotnet/csharp/language-reference/operators/conditional-operator
+// https://learn.microsoft.com/es-es/dotnet/csharp/language-reference/keywords/where-clause
+// https://www.geeksforgeeks.org/c-sharp/c-sharp-adding-the-elements-of-the-specified-collection-to-the-end-of-the-list/
+// https://learn.microsoft.com/en-us/dotnet/api/system.random.getitems?view=net-10.0#:~:text=In%20this%20article,length%20Int32
+// https://www.geeksforgeeks.org/c-sharp/c-sharp-boolean-tryparse-method/
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.WriteLine("Welcome to the Scripture Memorizer");
+        Console.WriteLine(new string('=', 40));
+        
+        GameManager game = new();
+        game.StartGame();
+
+        Console.WriteLine("Thank you for playing Scripture Memorizer!");
     }
 }
