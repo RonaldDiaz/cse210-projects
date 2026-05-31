5a7dc996f8e4476b5c12b63230fda9c1dcda3222
RonaldDiaz <ronalddiazz@gmail.com>
2026-05-30 23:43:45 -0400

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new("Ronald Diaz", "Programming with Classes");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new("Diego Diaz", "Calculus", "19", "19-25");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WrittingAssignment writtingAssignment = new("Verónica Hernández", "Contemporary History", "The Oil Nationalization");
+        Console.WriteLine(writtingAssignment.GetSummary());
+        Console.WriteLine(writtingAssignment.GetWrittingInformation());
     }
 }
